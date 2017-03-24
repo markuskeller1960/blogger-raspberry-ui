dc733283c7b3131b96ad3ea3ce8eb9652e791c83
Markus Keller <goofyworld88@gmail.com>
2017-03-24 09:14:59 +0100

    adds device information to navbar header

--- src/app/navigation/app.sidenav-header.component.ts
+++ src/app/navigation/app.sidenav-header.component.ts
@@ -1,4 +1,7 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ApplicationService } from '../service';
+import { DeviceIndexModel } from '../common';
 
 @Component({
   selector: 'app-main-sidenav-header',
@@ -6,11 +9,36 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['styles/app.sidenav-header.component.scss'],
 })
 
-export class MainSidenavHeaderComponent {
+export class MainSidenavHeaderComponent implements OnInit, OnDestroy {
+
+  private activeDeviceSubscription: Subscription;
+  private deviceActive = false;
+
+  activeDevice: DeviceIndexModel;
 
   @Output() onSideBarItemHeaderClicked: EventEmitter<any> = new EventEmitter();
 
+  constructor(private applicationService: ApplicationService) {}
+
+  ngOnInit() {
+    this.activeDeviceSubscription = this.applicationService.getConnectedDevice()
+      .subscribe((device) => this.initDevice(device));
+  }
+
+  ngOnDestroy() {
+    this.activeDeviceSubscription.unsubscribe();
+  }
+
   clickSidebarIcon() {
     this.onSideBarItemHeaderClicked.emit({value: 'MainSidenavHeaderComponent:sidebarItem:clicked'});
+  }
+
+  hasActiveDevice(): boolean {
+    return this.deviceActive;
+  }
+
+  private initDevice(device: DeviceIndexModel) {
+    this.activeDevice = device;
+    this.deviceActive = true;
   }
 }
